e36136d349b4104938a6672714850be17af2d745
eunewoo <87342625+eunewoo@users.noreply.github.com>
2023-05-29 19:04:29 +0900

    [Upgrade]Readapt high accuracy training model but now showing accuracy rate

--- OpenFace-master/pkdetector/detect-parkinsons-master/detect_parkinsons.py
+++ OpenFace-master/pkdetector/detect-parkinsons-master/detect_parkinsons.py
@@ -1,9 +1,4 @@
-#converted code state
-
-# USASGE
-# python detect_parkinsons.py --dataset dataset/spiral
-# python detect_parkinsons.py --dataset dataset/wave
-
+# All necessary import statements
 import numpy as np
 import cv2
 import os
@@ -53,58 +48,15 @@ print("[INFO] training model...")
 model = RandomForestClassifier(n_estimators=100)
 model.fit(trainX, trainY)
 
-print("[INFO] evaluating model...")
-predictions = model.predict(testX)
-
-cm = confusion_matrix(testY, predictions).flatten()
-(tn, fp, fn, tp) = cm
-acc = (tp + tn) / float(cm.sum())
-sensitivity = tp / float(tp + fn)
-specificity = tn / float(tn + fp)
-
-print(f"Accuracy: {acc:.4f}")
-
-testingPaths = list(paths.list_images(testingPath))
-idxs = np.random.choice(np.arange(0, len(testingPaths)), size=(25,), replace=False)
-images = []
-
-for i in idxs:
-    image = cv2.imread(testingPaths[i])
-    output = image.copy()
-    output = cv2.resize(output, (128, 128))
-
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.resize(image, (200, 200))
-    image = cv2.threshold(image, 0, 255,
-        cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-
-    features = quantify_image(image)
-    preds = model.predict([features])
-    label = le.inverse_transform(preds)[0]
-
-    color = (0, 255, 0) if label == "healthy" else (0, 0, 255)
-    cv2.putText(output, label, (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-        color, 2)
-    images.append(output)
-
-montage = build_montages(images, (128, 128), (5, 5))[0]
-
+# Load the new image and make the prediction
 new_image_path = 'dataset/spiral/testing/parkinson/V01PE01.png'
-
 print("[INFO] loading and preprocessing new image...")
-
 new_image = cv2.imread(new_image_path)
 new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
 new_image = cv2.resize(new_image, (200, 200))
 new_image = cv2.threshold(new_image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-
 new_features = quantify_image(new_image)
-
 print("[INFO] predicting for new image...")
 new_preds = model.predict([new_features])
 new_label = le.inverse_transform(new_preds)[0]
-
 print("The predicted label for the new image is: ", new_label)
-
-
-
