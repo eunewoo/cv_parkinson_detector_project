c181b7579a74a9cf7cc36a95d9a9a5e5aab7b17d
eunewoo <87342625+eunewoo@users.noreply.github.com>
2023-06-02 05:32:45 +0900

    [Add]added feature that front spiral detect button connects with spiralTest.py and works well

--- OpenFace-master/pkdetector/detect-parkinsons-master/spiralTest.py
+++ OpenFace-master/pkdetector/detect-parkinsons-master/spiralTest.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from imutils import paths
 import joblib  # used for loading sklearn models
+import json
 
 def quantify_image(image):
     features = feature.hog(image, orientations=9,
@@ -13,18 +14,23 @@ def quantify_image(image):
     return features
 
 # Load the trained model and label encoder
-model = joblib.load('trained_model.pkl')
-le = joblib.load('label_encoder.pkl')
+model = joblib.load('/Users/eunewoo/Desktop/2023Spring/CSE327/diary30_327front/OpenFace-master/pkdetector/detect-parkinsons-master/trained_model.pkl')
+le = joblib.load('/Users/eunewoo/Desktop/2023Spring/CSE327/diary30_327front/OpenFace-master/pkdetector/detect-parkinsons-master/label_encoder.pkl')
 
 # Load the new image and make the prediction
-new_image_path = input("Please enter the path of the new image: ")
-print("[INFO] loading and preprocessing new image...")
+new_image_path = '/Users/eunewoo/Desktop/2023Spring/CSE327/diary30_327front/OpenFace-master/pkdetector/sampleFront/uploads/spiralMe.png'
+# print("[INFO] loading and preprocessing new image...")
 new_image = cv2.imread(new_image_path)
 new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
 new_image = cv2.resize(new_image, (200, 200))
 new_image = cv2.threshold(new_image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 new_features = quantify_image(new_image)
-print("[INFO] predicting for new image...")
+# print("[INFO] predicting for new image...")
 new_preds = model.predict([new_features])
 new_label = le.inverse_transform(new_preds)[0]
-print("The predicted label for the new image is: ", new_label)
+# print("The predicted label for the new image is: ", new_label)
+
+result = {"message": new_label}
+
+with open('output.json', 'w') as f:
+    json.dump(result, f)
